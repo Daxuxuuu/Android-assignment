020b0e3b090e89084ca14cdd6b580d8a17b928c6
Daxuxuuu <104289422+Daxuxuuu@users.noreply.github.com>
2022-05-25 20:12:18 +0800

    Add files via upload
    update

---
+++ main/java/org/meicode/helloworld/Main.java
@@ -0,0 +1,7 @@
+package org.meicode.helloworld;
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
---
+++ main/java/org/meicode/helloworld/Phone.java
@@ -0,0 +1,13 @@
+package org.meicode.helloworld;
+
+public class Phone {
+    private String name;
+    int screensize;
+    int memoryRam;
+    int camera;
+
+    public void playmusic(String trackname){
+        System.out.println("Playing " + trackname);
+    }
+
+}
